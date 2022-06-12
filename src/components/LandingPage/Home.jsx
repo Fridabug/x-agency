bc40e06723091d08eaa1549c07339c17daccfc4d
Fridabug <friederikespring@posteo.de>
2022-06-12 16:16:06 +0200

    width of side and content

--- src/components/LandingPage/Home.jsx
+++ src/components/LandingPage/Home.jsx
@@ -3,8 +3,6 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
-        <p>I'm a p element</p>
-        <a>link about solutions webdevelopment </a>
     </div>
   )
 }
