e1f16991b7a1f05e12bea35c7c78311c3b660a56
Fridabug <friederikespring@posteo.de>
2022-06-12 15:42:57 +0200

    fonts and colors

--- src/components/LandingPage/Home.jsx
+++ src/components/LandingPage/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <p>I'm a p element</p>
+        <a>link about solutions webdevelopment </a>
+    </div>
   )
 }
