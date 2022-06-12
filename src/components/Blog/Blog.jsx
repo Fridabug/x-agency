471b69247827d13e62d80946b0a5553ed5fa9756
hudir <hudirybw@gmail.com>
2022-06-12 17:58:24 +0200

    added blogs and singleblog

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { Context } from '../../context/context'
+import SingleBlog from './SingleBlog'
 
 export default function Blog() {
+  const {blogs} = useContext(Context)
   return (
-    <div>Blog</div>
+    <div>
+      {/* {blogs && JSON.stringify(blogs)} */}
+      {blogs && blogs.map((x,i)=> (
+        <div key={i} className="card" style={{width: "18rem"}}>
+        <div className="card-body">
+          <h5 className="card-title">{x.title}</h5>
+          <div className='d-flex justify-content-between'>
+          <h6 className="card-subtitle mb-2 text-muted">{x.author}</h6>
+          <h6 className="card-subtitle mb-2 text-muted">{x.time}</h6>
+
+          </div>
+          
+          <p className="card-text pTagInBlogs">{x.body[0].section}</p>
+          <Link to={"/blog/"+x.id} element={ <SingleBlog /> }>Read More</Link>
+      
+        </div>
+      </div>
+      )  )}
+    </div>
   )
 }
--- src/context/context.js
+++ src/context/context.js
@@ -1,12 +1,21 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect, useState } from 'react';
+import blogData from "./json/blogs.json" 
 
 export const Context = createContext()
 
 export function ContextProvider({children}){
+    const [blogs, setBlogs]=useState([])
+
+    useEffect(()=>{
+        if(blogs.length===0) setBlogs(blogData)
+       
+    },[])
+
+    
 
 
 
     return ( <Context.Provider value={
-        0
+        blogs
     }>{children}</Context.Provider> )
 } 
