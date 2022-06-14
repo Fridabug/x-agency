110850163c6cd29c4b69d016ac125c8cbbada7c8
hudir <hudirybw@gmail.com>
2022-06-14 22:40:27 +0200

    awards basic done

--- src/components/Solutions/Solutions.jsx
+++ src/components/Solutions/Solutions.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Solutions() {
   return (
-    <div className='page-content'>Solutions</div>
+    <div className='page-content text-center'>
+
+        <h1>Welcome to Solutions Component </h1>
+        <p>let's make some gif and link of our Portfolio Page</p>
+        <p>or the picture of your Project/Work/Exercise</p>
+        <p>if you have a good idea about content of this page, just leave a massage here!</p>
+    </div>
   )
 }
