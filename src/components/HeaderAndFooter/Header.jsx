1c294959b552de517b7d625aaf6160409f7078e1
Fridabug <friederikespring@posteo.de>
2022-06-14 14:33:49 +0200

    fixing logo

--- src/components/HeaderAndFooter/Header.jsx
+++ src/components/HeaderAndFooter/Header.jsx
@@ -5,8 +5,9 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <header>
-      <Link to="./">
+      <Link to="./" className='logo-container'>
       <div className='logo'></div>
+      <h1> agency </h1>
       </Link>
         <Nav/>
     </header>
