0c83d0c2d7698e594123e2ed73cd57cc3b80138a
eszterk90git config --global user.email eszter.kadar1@gmail.com <eszter.kadar1@gmail.com>
2022-08-17 21:24:12 +0200

    about section created

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,6 +3,10 @@ import React from "react";
 export default function About() {
   return (
     <div className="page-content">
-    </div>
+      <h1>About us</h1>
+      <div>Our team provides personal, professional and friendly service which carefully considers your business goals and requirements. We transform innovative visions into brilliant realities. Got one? Let’s make outstanding work together!</div>
+     
+      
+  </div>
   );
 }
